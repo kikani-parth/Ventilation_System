5983c8c0c0acfb320fe21bdf232ae6effdd4c21b
Parth Kikani <parthkikani02@gmail.com>
2023-10-08 14:53:51 +0300

    implemented functionality in live.js for updating live page fields

--- public/js/live.js
+++ public/js/live.js
@@ -1,5 +1,18 @@
-const socket = new WebSocket('ws://localhost:3000');
+const socket = new WebSocket('ws://localhost:3000/live');
 
 socket.addEventListener('open', (event) => {
     console.log('WebSocket connection opened:', event);
+});
+
+socket.addEventListener('message', (event) => {
+    const data = JSON.parse(event.data);
+    const message = JSON.parse(data.message);
+
+    //Update the live page fields
+    document.getElementById('fanSpeed').textContent = message.speed;
+    document.getElementById('pressure').textContent = message.pressure;
+    document.getElementById('co2Level').textContent = message.co2;
+    document.getElementById('rh').textContent = message.rh;
+    document.getElementById('temperature').textContent = message.temp;
+
 });
