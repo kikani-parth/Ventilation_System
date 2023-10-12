1d88a241d9cda7500879cfebaa3096d5450815b2
Parth Kikani <parthkikani02@gmail.com>
2023-10-12 23:26:47 +0300

    implemented pagination for stacked bar graph and line graph

--- public/js/charts.js
+++ public/js/charts.js
@@ -1,22 +1,30 @@
 const socket = new WebSocket('ws://localhost:3000/charts');
 
 // Event listener for the Drop-down menu button
-let chartType, previousChartType;
+let chartType = 'bar', previousChartType, doughnutChart;
 document.getElementById('chartType').addEventListener('change', () => {
-    // Store the previous chart type value (if any)
-    previousChartType = chartType;
-
     // Get the selected chart type
     chartType = document.getElementById('chartType').value;
 
     // Toggle the display based on the chart type
     toggleInputDisplay();
 
+    // Empty chart type value (will get it again when update button is pressed)
+    chartType = undefined;
+
 });
 
 // Event listener for the "Update Chart" button
 document.getElementById('updateChart').addEventListener('click', () => {
-    if (chartType === 'doughnut') {
+    // Get the selected chart type
+    doughnutChart = document.getElementById('chartType').value;
+    if (doughnutChart === 'doughnut') {
+        // Hide the page navigation
+        document.getElementById('pagination').style.display = 'none';
+
+        // Update the previous chart type
+        previousChartType = doughnutChart;
+
         // Get nr value
         const nr = document.getElementById('nr').value;
 
@@ -36,6 +44,9 @@ document.getElementById('updateChart').addEventListener('click', () => {
         // Send the nr to the server
         socket.send(JSON.stringify(samplenr));
     } else {
+        // Store the previous chart type value
+        previousChartType = chartType;
+
         // Get the selected chart type
         chartType = document.getElementById('chartType').value;
 
@@ -95,10 +106,9 @@ function toggleInputDisplay() {
     }
 }
 
-let mongoDBData, chartData, chart;
+let chartData, rawChartData, lineChartData, doughnutChartData, chart;
 socket.addEventListener('message', (event) => {
     chartData = JSON.parse(event.data);
-
     // If it is an MQTT message, exit the function
     if ('topic' in chartData) {
         return;
@@ -116,20 +126,15 @@ socket.addEventListener('message', (event) => {
         alert('No data available for the requested sample number!');
         return;
     }
+    rawChartData = chartData;
+    lineChartData = chartData;
+    doughnutChartData = chartData;
 
-    /*
-    // Check if chartData is an object and wrap it in an array if needed
-    if (Array.isArray(chartData)) {
-        mongoDBData = chartData;
-    } else {
-        mongoDBData = [chartData];
-    }
-*/
     // If a chart does not exist, create a new chart. Otherwise, update the existing chart
     if (!chart) {
         createChart();
     } else {
-        updateChart();
+        updateChart(chartData);
     }
 
 });
@@ -159,53 +164,54 @@ function createChart() {
         case 'line':
             createLineChart();
             break;
-        case 'doughnut':
-            createDoughnutChart();
-            break;
+        //case 'doughnut':
+        // createDoughnutChart();
+        // break;
         default:
-            console.error('Invalid chart type:', chartType);
+            createDoughnutChart();
+            //console.error('Invalid chart type:', chartType);
             return;
     }
 }
 
-function createBarChart() {
+function newBarChart(data) {
     let delayed;
     chart = new Chart(canvasElement, {
         type: 'bar',
         data: {
-            labels: chartData.map(item => item.nr),
+            labels: data.map(item => item.nr),
             datasets: [
                 {
                     label: 'Speed',
-                    data: chartData.map(item => item.speed),
+                    data: data.map(item => item.speed),
                     backgroundColor: 'rgba(255, 99, 132, 0.2)',
                     borderColor: 'rgba(255, 99, 132, 1)',
                     borderWidth: 1
                 },
                 {
                     label: 'Pressure',
-                    data: chartData.map(item => item.pressure),
+                    data: data.map(item => item.pressure),
                     backgroundColor: 'rgba(54, 162, 235, 0.2)',
                     borderColor: 'rgba(54, 162, 235, 1)',
                     borderWidth: 1
                 },
                 {
                     label: 'CO2',
-                    data: chartData.map(item => item.co2),
+                    data: data.map(item => item.co2),
                     backgroundColor: 'rgba(255, 206, 86, 0.2)',
                     borderColor: 'rgba(255, 206, 86, 1)',
                     borderWidth: 1
                 },
                 {
                     label: 'RH',
-                    data: chartData.map(item => item.rh),
+                    data: data.map(item => item.rh),
                     backgroundColor: 'rgba(75, 192, 192, 0.2)',
                     borderColor: 'rgba(75, 192, 192, 1)',
                     borderWidth: 1
                 },
                 {
                     label: 'Temperature',
-                    data: chartData.map(item => item.temp),
+                    data: data.map(item => item.temp),
                     backgroundColor: 'rgba(153, 102, 255, 0.2)',
                     borderColor: 'rgba(153, 102, 255, 1)',
                     borderWidth: 1
@@ -241,43 +247,78 @@ function createBarChart() {
     })
 }
 
-function createLineChart() {
+let currentPage = 1;        // Current page number
+const itemsPerPage = 25;    // Number of items to display per page
+function renderPartialChart() {
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+
+    // Display the initially hidden page navigation buttons
+    document.getElementById('pagination').style.display = 'block';
+
+    // Divide the chartData array into a smaller array whose length = 25
+    const slicedData = chartData.slice(startIndex, endIndex);
+
+    if (chartType === 'bar') {
+        newBarChart(slicedData);
+    } else {
+        newLineChart(slicedData);
+    }
+}
+
+function updatePartialChart() {
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const slicedData = rawChartData.slice(startIndex, endIndex);
+
+    updateChart(slicedData);
+}
+
+function createBarChart() {
+    if (chartData.length > 25) {
+        renderPartialChart();
+    } else {
+        newBarChart(rawChartData);
+    }
+}
+
+function newLineChart(data) {
     chart = new Chart(canvasElement, {
         type: 'line',
         data: {
-            labels: chartData.map(item => item.nr),
+            labels: data.map(item => item.nr),
             datasets: [
                 {
                     label: 'Speed',
-                    data: chartData.map(item => item.speed),
+                    data: data.map(item => item.speed),
                     borderColor: 'rgba(255, 99, 132, 1)',
                     borderWidth: 2,
                     fill: false
                 },
                 {
                     label: 'Pressure',
-                    data: chartData.map(item => item.pressure),
+                    data: data.map(item => item.pressure),
                     borderColor: 'rgba(54, 162, 235, 1)',
                     borderWidth: 2,
                     fill: false
                 },
                 {
                     label: 'CO2',
-                    data: chartData.map(item => item.co2),
+                    data: data.map(item => item.co2),
                     borderColor: 'rgba(255, 206, 86, 1)',
                     borderWidth: 2,
                     fill: false
                 },
                 {
                     label: 'RH',
-                    data: chartData.map(item => item.rh),
+                    data: data.map(item => item.rh),
                     borderColor: 'rgba(75, 192, 192, 1)',
                     borderWidth: 2,
                     fill: false
                 },
                 {
                     label: 'Temperature',
-                    data: chartData.map(item => item.temp),
+                    data: data.map(item => item.temp),
                     borderColor: 'rgba(153, 102, 255, 1)',
                     borderWidth: 2,
                     fill: false
@@ -286,15 +327,28 @@ function createLineChart() {
         },
         options: {
             scales: {
+                aspectRatio: 3,
+                maintainAspectRatio: false,
                 x: {
                     beginAtZero: true
                 },
+                /*
                 y: {
-                    beginAtZero: true
+                    beginAtZero: false
                 }
+                */
             }
         }
     });
+
+}
+
+function createLineChart() {
+    if (chartData.length > 25) {
+        renderPartialChart();
+    } else {
+        newLineChart(lineChartData);
+    }
 }
 
 function createDoughnutChart() {
@@ -304,11 +358,11 @@ function createDoughnutChart() {
             labels: ['Speed', 'Pressure', 'CO2', 'RH', 'Temperature'],
             datasets: [
                 {
-                    data: [chartData.speed,
-                        chartData.pressure,
-                        chartData.co2,
-                        chartData.rh,
-                        chartData.temp,
+                    data: [doughnutChartData.speed,
+                        doughnutChartData.pressure,
+                        doughnutChartData.co2,
+                        doughnutChartData.rh,
+                        doughnutChartData.temp,
                     ],
                     backgroundColor: [
                         'rgba(255, 99, 132, 1)',
@@ -327,22 +381,16 @@ function createDoughnutChart() {
     });
 }
 
-function updateChart() {
-    /*
-    // Extract the relevant data for the chart
-    chartData = mongoDBData.map(item => ({
-        nr: item.nr,
-        speed: item.speed,
-        pressure: item.pressure,
-        co2: item.co2,
-        rh: item.rh,
-        temp: item.temp
-    }));
-*/
+function updateChart(data) {
     // Check if chart type has changed
     if (chartType !== previousChartType) {
         // Remove existing chart
         chart.destroy();
+
+        // Reset page number before creating new chart
+        currentPage = 1;
+        // Update the correct Page number
+        document.getElementById('currentPage').textContent = 'Page ' + currentPage;
 
         // Create new chart based on the selected type
         switch (chartType) {
@@ -352,32 +400,50 @@ function updateChart() {
             case 'line':
                 createLineChart();
                 break;
-            case 'doughnut':
-                createDoughnutChart();
-                break;
+            //case 'doughnut':
+            // createDoughnutChart();
+            // break;
             default:
-                console.error('Invalid chart type:', chartType);
+                createDoughnutChart();
+                //console.error('Invalid chart type:', chartType);
                 return;
         }
     }
     // If chart type has not changed, update the existing chart with new datasets
     else {
-        if (chartType === 'doughnut') {
+        if (doughnutChart === 'doughnut') {
             // Update the chart data (for doughnut chart)
-            chart.data.datasets[0].data = [chartData.speed,
-                chartData.pressure,
-                chartData.co2,
-                chartData.rh,
-                chartData.temp,
+            chart.data.datasets[0].data = [doughnutChartData.speed,
+                doughnutChartData.pressure,
+                doughnutChartData.co2,
+                doughnutChartData.rh,
+                doughnutChartData.temp,
             ];
         } else {
-            // Update the chart data (for bar chart or line chart)
-            chart.data.labels = chartData.map(item => item.nr);
-            chart.data.datasets[0].data = chartData.map(item => item.speed);
-            chart.data.datasets[1].data = chartData.map(item => item.pressure);
-            chart.data.datasets[2].data = chartData.map(item => item.co2);
-            chart.data.datasets[3].data = chartData.map(item => item.rh);
-            chart.data.datasets[4].data = chartData.map(item => item.temp);
+            if (data.length > 25) {
+                currentPage = 1;
+                updatePartialChart();
+            } else {
+                if (rawChartData.length <= 25) {
+                    // Hide the page navigation
+                    document.getElementById('pagination').style.display = 'none';
+                } else {
+                    // Display the initially hidden page navigation buttons
+                    document.getElementById('pagination').style.display = 'block';
+
+                    // Update the correct Page number
+                    document.getElementById('currentPage').textContent = 'Page ' + currentPage;
+
+                }
+                // Update the chart data (for bar chart or line chart)
+                chart.data.labels = data.map(item => item.nr);
+                chart.data.datasets[0].data = data.map(item => item.speed);
+                chart.data.datasets[1].data = data.map(item => item.pressure);
+                chart.data.datasets[2].data = data.map(item => item.co2);
+                chart.data.datasets[3].data = data.map(item => item.rh);
+                chart.data.datasets[4].data = data.map(item => item.temp);
+            }
+
         }
 
         // Update the previous chart
@@ -385,3 +451,28 @@ function updateChart() {
     }
 }
 
+function goToPage(pageNumber) {
+    currentPage = pageNumber;
+    updatePartialChart();
+}
+
+// Add event listeners for pagination buttons
+document.getElementById('prevPage').addEventListener('click', () => {
+    if (currentPage > 1) {
+        // Store the previous chart type value
+        previousChartType = chartType;
+        goToPage(currentPage - 1);
+        document.getElementById('currentPage').textContent = 'Page ' + currentPage;
+    }
+});
+document.getElementById('nextPage').addEventListener('click', () => {
+    const totalPages = Math.ceil(rawChartData.length / itemsPerPage);    // Total number of pages where chart will be displayed
+
+    if (currentPage < totalPages) {
+        // Store the previous chart type value
+        previousChartType = chartType;
+        goToPage(currentPage + 1);
+        document.getElementById('currentPage').textContent = 'Page ' + currentPage;
+
+    }
+});
